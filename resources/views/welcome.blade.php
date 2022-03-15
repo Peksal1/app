04a87318d2745e4ecaa8a9bcf35435c3bbe08cff
Peksal1 <laurs2000@gmail.com>
2022-03-15 19:12:50 +0000

    added a favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+    <link rel="shortcut icon" type="image/png" href="/utility/favicon.png" />
     <title>Aleksandras Laurs</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <!-- Fonts -->
